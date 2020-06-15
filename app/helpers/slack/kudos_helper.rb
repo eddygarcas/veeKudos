8de9563df52cd063a1bd9261daa6c88cc0aa0618
Eduard <edugarcas@gmail.com>
2020-06-15 14:33:36 +0200

    First veeKudos version

--- app/helpers/slack/kudos_helper.rb
+++ app/helpers/slack/kudos_helper.rb
@@ -7,7 +7,7 @@ module Slack::KudosHelper
 
   def self.get_giphy
     instance = GiphyClient::DefaultApi.new
-    data = instance.gifs_search_get(ENV[:GIPHY_API_KEY],'thank you',{limit: 1})
+    data = instance.gifs_search_get(ENV["GIPHY_API_KEY"],'thank you',{limit: 1})
     data.data[0].embed_url
   end
 
