2b83cce6ab399461040b3911d1889acd66483868
Eduard <edugarcas@gmail.com>
2020-06-16 13:06:57 +0200

    Add giphy categoery to display animated gifs accordingly to the user kudo text.

--- app/helpers/slack/kudos_helper.rb
+++ app/helpers/slack/kudos_helper.rb
@@ -5,13 +5,6 @@ module Slack::KudosHelper
   class KudosFormatError < StandardError;end
   class LeadersFormatError < StandardError;end
 
-  # awesome
-  # well done
-  # proud
-  # happy
-  # great job
-  # congratulations
-  # many thanks
   def self.get_giphy text = "great job"
     instance = GiphyClient::DefaultApi.new
     data = instance.gifs_search_get(ENV["GIPHY_API_KEY"],kudo_category(text),{limit: 25})
@@ -19,9 +12,7 @@ module Slack::KudosHelper
   end
 
   def self.kudo_category text
-    adj = ["awesome","well done","proud","great job","congratulation","thanks"].select {|elem| text.downcase.include? elem }
-    pp adj
-    adj[0]
+    VeeKudos.config[:kudo_categories].select {|elem| text.downcase.include? elem }[0]
   end
 
 end
--- config/application.rb
+++ config/application.rb
@@ -28,7 +28,9 @@ module VeeKudos
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-
+    def self.config
+      Rails.application.config_for(:veekudos)
+    end
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
