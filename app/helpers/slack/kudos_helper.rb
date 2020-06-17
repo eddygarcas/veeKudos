1b2c3c33bd02bbf8b245e741e9cfaf3c992d8b70
Eduard <edugarcas@gmail.com>
2020-06-17 08:35:36 +0200

    Add giphy categoery to display animated gifs accordingly to the user kudo text.

--- app/helpers/slack/kudos_helper.rb
+++ app/helpers/slack/kudos_helper.rb
@@ -5,14 +5,14 @@ module Slack::KudosHelper
   class KudosFormatError < StandardError;end
   class LeadersFormatError < StandardError;end
 
-  def self.get_giphy text = "great job"
+  def self.get_giphy text = ""
     instance = GiphyClient::DefaultApi.new
-    data = instance.gifs_search_get(ENV["GIPHY_API_KEY"],kudo_category(text),{limit: 25})
+    data = instance.gifs_search_get(ENV["GIPHY_API_KEY"],kudo_category(text) {|elem | elem.blank? ? "great job" : elem},{limit: 25})
     data.data[rand(25)].images.downsized.url
   end
 
   def self.kudo_category text
-    VeeKudos.config[:kudo_categories].select {|elem| text.downcase.include? elem }[0]
+    yield VeeKudos.config[:kudo_categories].select {|elem| text.downcase.include? elem }[0]
   end
 
 end
