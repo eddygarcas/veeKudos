1c803cf344ce213345bb391c63e7edc80633a0ac
Eduard <edugarcas@gmail.com>
2020-06-22 11:17:19 +0200

    Add giphy categoery to display animated gifs accordingly to the user kudo text.

--- app/helpers/slack/kudos_helper.rb
+++ app/helpers/slack/kudos_helper.rb
@@ -1,25 +1,27 @@
 require 'GiphyClient'
 
-
 module Slack::KudosHelper
-  class KudosFormatError < StandardError;end
-  class LeadersFormatError < StandardError;end
+  class KudosFormatError < StandardError;
+  end
+  class LeadersFormatError < StandardError;
+  end
 
   def self.get_giphy text = ""
-    instance = GiphyClient::DefaultApi.new
-    data = instance.gifs_search_get(ENV["GIPHY_API_KEY"],kudo_category(text) {|elem | elem.blank? ? "great job" : elem},{limit: 25})
-    data.data[rand(25)].images.downsized.url
+    category = kudo_category text {|elem| elem.blank? ? "great job" : elem}
+    Rails.cache.fetch("get_giphy_#{category}", expires_in: 5.day) {
+      GiphyClient::DefaultApi.new.gifs_search_get(ENV["GIPHY_API_KEY"], category, {limit: 25})
+    }[rand(25)].images.downsized.url
   end
 
   def self.kudo_category text
-    yield VeeKudos.config[:kudo_categories].select {|elem| text.downcase.include? elem }[0]
+    yield VeeKudos.config[:kudo_categories].select {|elem| text.downcase.include? elem}[0]
   end
 
   Action = Struct.new(:operation) do
     def === (param)
       case operation
       when "leader"
-        ["getter","giver"].include? param.to_s.downcase
+        ["getter", "giver"].include? param.to_s.downcase
       when "kudo"
         param.to_s.downcase.include? "@"
       when "list"
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,8 +52,8 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "veeKudos_production"
-
-  config.action_mailer.perform_caching = false
+  config.consider_all_requests_local       = false
+  config.action_mailer.perform_caching = true
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
