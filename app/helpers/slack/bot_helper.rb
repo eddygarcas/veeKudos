cd30cfe3f3385b3daeffa1c6e5ceed5d5b23aa40
Eduard <edugarcas@gmail.com>
2020-06-25 13:38:39 +0200

    Add giphy categoery to display animated gifs accordingly to the user kudo text.

--- app/helpers/slack/bot_helper.rb
+++ app/helpers/slack/bot_helper.rb
@@ -1,19 +1,20 @@
-
 module Slack::BotHelper
   class Herald
     include HTTParty
 
+    attr_reader :xoxb_token
+
     format :json
+    xoxb_token ENV[:XOXB_TOKEN.to_s]
 
     def initialize(uri = VeeKudos.config[:slack_api][:base_uri])
       self.class.base_uri uri
     end
 
     def send_message_to channel_id, text
-      pp self.class.base_uri + VeeKudos.config[:slack_api][:postMessage]
       response = self.class.post(
           VeeKudos.config[:slack_api][:postMessage],
-          headers: {"Authorization" => "Bearer #{ENV[:XOXB_TOKEN.to_s]}"},
+          headers: {"Authorization" => "Bearer #{xoxb_token}"},
           body: {channel: channel_id,text: text})
       pp response
     end
