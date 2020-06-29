b72d44c2d818f1237f27a72b9df6a43178f28e43
Eduard <edugarcas@gmail.com>
2020-06-29 15:02:00 +0200

    Add giphy categoery to display animated gifs accordingly to the user kudo text.

--- app/models/workspace.rb
+++ app/models/workspace.rb
@@ -1,4 +1,14 @@
 class Workspace < ApplicationRecord
-  has_many :kudos
+  has_many :kudos, dependent: :destroy
 
+  def self.create params
+    Workspace.where(team_id: params[:team_id]).
+        first_or_create(Workspace.parse(params).
+            update(Workspace.parse(params)))
+  end
+
+  def self.parse params
+    {team_domain: params[:team_domain],
+     team_id: params[:team_id]}
+  end
 end
