dc97d7e424d924231b0734e004cf6810f198ddd6
Eduard <edugarcas@gmail.com>
2020-06-30 16:53:34 +0200

    Add giphy categoery to display animated gifs accordingly to the user kudo text.

--- app/jobs/kudos_job.rb
+++ app/jobs/kudos_job.rb
@@ -4,10 +4,8 @@ class KudosJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    @commands = []
-    @commands[0] = ""
     @kudos = Kudo.getter_leader("T0F48V7E2").sort_by {|_k, v| v}.reverse
     Herald.new(VeeKudos.config[:web_hooks][:base_uri]).
-        send_to_webhook(Slack::KudosController.render(:leaders, assigns: {commands: @commands, kudos: @kudo}))
+        send_to_webhook(Slack::KudosController.render(:leaders, assigns: {commands: "", kudos: @kudos}))
   end
 end
